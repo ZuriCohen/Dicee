351ed8124652cca2350e404ad2e03b6aff7c1bbe
zuriCohen <elco2365>
2018-06-20 09:52:09 +0300

    I add an image array and a random generator and the app is ready.

--- app/src/main/java/com/seal/dicee/MainActivity.java
+++ app/src/main/java/com/seal/dicee/MainActivity.java
@@ -1,7 +1,14 @@
 package com.seal.dicee;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = findViewById(R.id.btnRoll);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Dicee" , "Button have pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee - random number", "the random number is " + number);
+
+                leftDice.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+
+                rightDice.setImageResource(diceArray[number]);
+
+
+
+
+
+
+
+            }
+        });
     }
 }
